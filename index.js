d7c13715a823ef99bd660ac886ba5fdb5d88568d
Ronnie Ganot <rganot@aligntech.com>
2020-02-11 16:13:43 +0200

    dbServices in correct place, working!

--- index.js
+++ index.js
@@ -1,81 +1,88 @@
-//TODO use import instead of require
-const express = require("express");
-const cors = require("cors");
+const dbService = require('./services/db/dbService');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(cors());
 
 app.listen(3333, () => {
-  console.log("Server running on port 3333");
+  console.log('Server running on port 3333');
 });
 
-app.get("/groups", (req, res, next) => {
-  res.json(["Web", "Core", "Scanner"]);
+app.get('/groups', async (req, res, next) => {
+  try {
+    const groups = await dbService.getGroups();
+    res.json(groups);
+  } catch (e) {
+    console.log('/groups failed! because: ', e);
+    res.json({}); //TODO convert to error in all get functions
+  }
 });
 
-const devDetails = [
-  { name: "Spiders", group: "Web", devs: ["shay-FE", "lior-BE"] },
-  { name: "Sharks", group: "Web", devs: ["Jenny-FE", "Shanni-BE"] },
-  { name: "Threads", group: "Web", devs: ["Tolik-BE", "Daniel-FE"] },
-  { name: "Gold Strikers", group: "Core", devs: [] },
-  { name: "Goblins", group: "Core", devs: [] },
-  { name: "Blues", group: "Core", devs: [] },
-  { name: "Seals", group: "Scanner", devs: [] },
-  { name: "Team 13", group: "Scanner", devs: [] }
-];
-
-app.get("/teams/withDevs", (req, res, next) => {
-  res.json(devDetails);
+app.get('/teams', async (req, res, next) => {
+  try {
+    const devDetails = await dbService.getDevsWithDetails();
+    res.json(devDetails.map(dev => ({ name: dev.name, group: dev.group })));
+  } catch (e) {
+    console.log('/teams failed! because: ', e);
+    res.json({});
+  }
 });
 
-app.get("/teams", (req, res, next) => {
-  res.json(devDetails.map(dev => ({ name: dev.name, group: dev.group })));
+app.get('/teams/withDevs', async (req, res, next) => {
+  try {
+    const devDetails = await dbService.getDevsWithDetails();
+    res.json(devDetails);
+  } catch (e) {
+    console.log('/teams/withDevs failed! because: ', e);
+    res.json({});
+  }
 });
 
-app.get("/releases", (req, res, next) => {
-  res.json([
-    { name: "20A5", startDate: "2/2/2020", endDate: "1/3/2020" },
-    { name: "20B", startDate: "14/2/2020", endDate: "1/4/2020" },
-    { name: "20C", startDate: "2/4/2020", endDate: "1/8/2020" }
-  ]);
+app.get('/releases', async (req, res, next) => {
+  try {
+    const releases = await dbService.getReleases();
+    res.json(releases);
+  } catch (e) {
+    console.log('/releases failed! because: ', e);
+    res.json({});
+  }
+});
+
+app.get('/devs', async (req, res, next) => {
+  try {
+    const devsCapacity = await dbService.getDevsCapacity();
+    res.json(devsCapacity);
+  } catch (e) {
+    console.log('/devs failed! because: ', e);
+    res.json({});
+  }
+});
+
+app.get('/devs&team=:team', async (req, res, next) => {
+  try {
+    const devsCapacity = await dbService.getDevsCapacity();
+    const devsOnSpecificTeam = devsCapacity.filter(dev => dev.team.toLowerCase() === req.params['team'].toLowerCase());
+    res.json(devsOnSpecificTeam);
+  } catch (e) {
+    console.log('/devs failed! because: ', e);
+    res.json({});
+  }
+});
+
+app.get('/weekDates', async (req, res, next) => {
+  //TODO this should be calculated, depending on the earliest release, and the latest. 
+  // returned value should look like {startingDate: '1/1/2020', weekNumber:1}. 
+  //client should expect that object, instead of currently array of w?. 
+  res.json(['w5', 'w6', 'w7', 'w8', 'w9', 'w10', 'w11', 'w12']);
 });
 
 // prettier-ignore
-const devs = [
-      { name: "Shay-FE", team: "Spiders", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
-      { name: "lior-BE", team: "Spiders", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
-      { name: "Jenny-FE", team: "Sharks", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
-      { name: "Shanni-BE", team: "Sharks", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
-      { name: "Tolik-BE", team: "Threads", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
-      { name: "Daniel-FE", team: "Threads", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} }
-];
-
-app.get("/devs", (req, res, next) => {    
-    res.json(devs)
-});
-
-app.get("/devs&team=:team", (req, res, next) => {    
-    const devsOnSpecificTeam = devs.filter(dev => dev.team.toLowerCase() === req.params["team"].toLowerCase())
-    res.json(devsOnSpecificTeam)
-});
-
-app.get("/weekDates", (req, res, next) => {
-  res.json(["w5", "w6", "w7", "w8", "w9", "w10", "w11", "w12"]);
-});
-
-// prettier-ignore
-app.get("/epics", (req, res, next) => {
-    res.json([
-      { name: 'Snapshot wave 2',             shortName: 'snapshot w2', release:'20B' , priority:'100', program:'ortho', estimations:{FE:{est:'15', max_parallel:'1'}, BE:{est:'6', max_parallel:'1'}, Core:{est:'5', max_parallel:'1'}, Scanner:{est:'0', max_parallel:'0'}, MSK:{est:'0', max_parallel:'0'}, ALG:{est:'0', max_parallel:'0'}}, candidate_teams: ['Spiders','Gold Strikers']},
-      { name: 'Patient-management with IDS', shortName: 'patient-mng', release:'20B' , priority:'200', program:'ortho', estimations:{FE:{est:'10', max_parallel:'1'}, BE:{est:'15', max_parallel:'1'}, Core:{est:'7', max_parallel:'1'}, Scanner:{est:'0', max_parallel:'1'}, MSK:{est:'0', max_parallel:'1'}, ALG:{est:'0', max_parallel:'1'}}, candidate_teams: ['Sharks','Gold Strikers']},
-      { name: 'Texture mapping',             shortName: 'texture'    , release:'20B' , priority:'300', program:'ortho', estimations:{FE:{est:'10', max_parallel:'1'}, BE:{est:'0',  max_parallel:'1'}, Core:{est:'10',max_parallel:'1'}, Scanner:{est:'0', max_parallel:'1'}, MSK:{est:'0', max_parallel:'1'}, ALG:{est:'0', max_parallel:'1'}}, candidate_teams: ['Spiders','Gold Strikers']},
-      { name: 'Account management',          shortName: 'accnt-mng'  , release:'20A5', priority:'50' , program:'ortho', estimations:{FE:{est:'15', max_parallel:'1'}, BE:{est:'15', max_parallel:'1'}, Core:{est:'0', max_parallel:'0'}, Scanner:{est:'0', max_parallel:'0'}, MSK:{est:'0', max_parallel:'0'}, ALG:{est:'0', max_parallel:'0'}}, candidate_teams: ['Spiders']},
-    ]);
+app.get("/epics", async (req, res, next) => {
+  try {
+    const epics = await dbService.getEpics();
+    res.json(epics);
+  } catch (e) {
+    console.log('/epics failed! because: ', e);
+    res.json({});
+  } 
   });
-
-/*
-getTeams, //group it belongs to, devs and their skill sets
-getReleases
-getDev  (with name+skillset, team, capacity). can get all devs in a team with param
-getWeekDates
-epics (name, short name, priority, program, efforts for all skillsets, prefered team to implement)
-*/
---
+++ services/db/dbService.js
@@ -0,0 +1,201 @@
+let firebase = require('firebase-admin');
+let serviceAccount = require('./serviceAccountKey.json');
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://releaseplanner-902dc.firebaseio.com'
+});
+
+let db = firebase.database();
+
+//#region getters
+
+const getGroups = async () => {
+  let data = await db.ref('groups').once('value');
+  return data.val();
+};
+
+const getDevsWithDetails = async () => {
+  let data = await db.ref('teams').once('value');
+  return data.val();
+};
+
+const getReleases = async () => {
+  let data = await db.ref('releases').once('value');
+  return data.val();
+};
+
+const getDevsCapacity = async () => {
+  let data = await db.ref('devs').once('value');
+  return data.val();
+};
+
+const getEpics = async () => {
+  let data = await db.ref('epics').once('value');
+  return data.val();
+};
+
+//#endregion getters
+//#region initializing the DB
+
+const resetToInitialDB = async () => {
+  setInitialGroups();
+  setInitialTeams();
+  setInitialDevs();
+  setInitialReleases();
+  setInitialEpics();
+};
+
+const setInitialGroups = async () => {
+  let ref = db.ref('groups');
+  await ref.set(['core', 'web', 'scanner']);
+};
+
+const setInitialTeams = async () => {
+  const devDetails = [
+    { name: 'Spiders', group: 'Web', devs: ['shay-FE', 'lior-BE'] },
+    { name: 'Sharks', group: 'Web', devs: ['Jenny-FE', 'Shanni-BE'] },
+    { name: 'Threads', group: 'Web', devs: ['Tolik-BE', 'Daniel-FE'] },
+    { name: 'Gold Strikers', group: 'Core', devs: [] },
+    { name: 'Goblins', group: 'Core', devs: [] },
+    { name: 'Blues', group: 'Core', devs: [] },
+    { name: 'Seals', group: 'Scanner', devs: [] },
+    { name: 'Team 13', group: 'Scanner', devs: [] }
+  ];
+
+  let ref = db.ref('teams');
+  await ref.set(devDetails);
+};
+
+const setInitialDevsCapacity = async () => {
+  // prettier-ignore
+  const devs = [
+    { name: "Shay-FE", team: "Spiders", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
+    { name: "lior-BE", team: "Spiders", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
+    { name: "Jenny-FE", team: "Sharks", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
+    { name: "Shanni-BE", team: "Sharks", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
+    { name: "Tolik-BE", team: "Threads", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} },
+    { name: "Daniel-FE", team: "Threads", capacity: {w5:"5", w6:"5", w7:"5", w8:"5", w9:"5", w10:"5", w11:"5", w12:"5"} }
+  ];
+  let ref = db.ref('devs');
+  await ref.set(devs);
+};
+
+const setInitialReleases = async () => {
+  const releases = [
+    { name: '20A5', startDate: '2/2/2020', endDate: '1/3/2020' },
+    { name: '20B', startDate: '14/2/2020', endDate: '1/4/2020' },
+    { name: '20C', startDate: '2/4/2020', endDate: '1/8/2020' }
+  ];
+  let ref = db.ref('releases');
+  await ref.set(releases);
+};
+
+const setInitialEpics = async () => {
+  const epics = [
+    {
+      name: 'Snapshot wave 2',
+      shortName: 'snapshot w2',
+      release: '20B',
+      priority: '100',
+      program: 'ortho',
+      estimations: {
+        FE: { est: '15', max_parallel: '1' },
+        BE: { est: '6', max_parallel: '1' },
+        Core: { est: '5', max_parallel: '1' },
+        Scanner: { est: '0', max_parallel: '0' },
+        MSK: { est: '0', max_parallel: '0' },
+        ALG: { est: '0', max_parallel: '0' }
+      },
+      candidate_teams: ['Spiders', 'Gold Strikers']
+    },
+    {
+      name: 'Patient-management with IDS',
+      shortName: 'patient-mng',
+      release: '20B',
+      priority: '200',
+      program: 'ortho',
+      estimations: {
+        FE: { est: '10', max_parallel: '1' },
+        BE: { est: '15', max_parallel: '1' },
+        Core: { est: '7', max_parallel: '1' },
+        Scanner: { est: '0', max_parallel: '1' },
+        MSK: { est: '0', max_parallel: '1' },
+        ALG: { est: '0', max_parallel: '1' }
+      },
+      candidate_teams: ['Sharks', 'Gold Strikers']
+    },
+    {
+      name: 'Texture mapping',
+      shortName: 'texture',
+      release: '20B',
+      priority: '300',
+      program: 'ortho',
+      estimations: {
+        FE: { est: '10', max_parallel: '1' },
+        BE: { est: '0', max_parallel: '1' },
+        Core: { est: '10', max_parallel: '1' },
+        Scanner: { est: '0', max_parallel: '1' },
+        MSK: { est: '0', max_parallel: '1' },
+        ALG: { est: '0', max_parallel: '1' }
+      },
+      candidate_teams: ['Spiders', 'Gold Strikers']
+    },
+    {
+      name: 'Account management',
+      shortName: 'accnt-mng',
+      release: '20A5',
+      priority: '50',
+      program: 'ortho',
+      estimations: {
+        FE: { est: '15', max_parallel: '1' },
+        BE: { est: '15', max_parallel: '1' },
+        Core: { est: '0', max_parallel: '0' },
+        Scanner: { est: '0', max_parallel: '0' },
+        MSK: { est: '0', max_parallel: '0' },
+        ALG: { est: '0', max_parallel: '0' }
+      },
+      candidate_teams: ['Spiders']
+    }
+  ];
+  let ref = db.ref('epics');
+  await ref.set(epics);
+};
+
+//#endregion initializing the DB
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////  temp stuff, remove later  /////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const f = async () => {
+//   await setInitialGroups();
+//   await setInitialTeams();
+//   await setInitialReleases();
+//   await setInitialDevsCapacity();
+//   await setInitialEpics();
+
+//   // await ref.child('users').set({ some: 'obj', foo: 'bar' });
+
+//   // await ref.once('value', snapshot => {console.log(snapshot.val())});
+
+//   // let updates = {};
+//   // updates['/users/some'] = 'ANOTHER OBJECT';
+//   // await ref.update(updates);
+
+//   // await ref.once('value', snapshot => {console.log(snapshot.val());
+//   // });
+// };
+
+// f();
+
+module.exports = {
+  resetToInitialDB,
+  getGroups,
+  getDevsWithDetails,
+  getDevsCapacity,
+  getReleases,
+  getEpics
+};
